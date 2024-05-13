66cd2fadd35ce518831eb7d4c468b6c52d7ffdd3
Tirths101 soni <tirthsoni56@gmail.com>
2024-05-13 19:00:26 +0000

    Add game.js code

--- game.js
+++ game.js
@@ -1,3 +1,12 @@
 const question = document.getElementById("question");
-const choices = document.getElementsByClassName("choice-text");
-console.log(choices);
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [
+    
+]
