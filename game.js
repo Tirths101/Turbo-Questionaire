64604a268073b8435098f8b68fefae558d3ab72c
Tirths101 soni <tirthsoni56@gmail.com>
2024-05-13 19:52:03 +0000

    Added questions to game.js

--- game.js
+++ game.js
@@ -8,5 +8,71 @@ let questionCounter = 0;
 let availableQuestions = [];
 
 let questions = [
-    
-]
+    {
+        question: 'Which country does BMW originate from?',
+        choice1: 'England',
+        choice2: 'Germany',
+        choice3: 'France',
+        choice4: 'Italy',
+        answer: 2,
+    },
+    {
+        question: 'Which country does Rolls Royce originate from?',
+        choice1: 'England',
+        choice2: 'Germany',
+        choice3: 'France',
+        choice4: 'Italy',
+        answer: 1,
+    },
+    {
+        question: 'Which country does Alfa Romeo originate from?',
+        choice1: 'England',
+        choice2: 'Germany',
+        choice3: 'France',
+        choice4: 'Italy',
+        answer: 4,
+    },
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuestions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //Go to the end page
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+    currentQuestion = availableQuestions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
+startGame()
