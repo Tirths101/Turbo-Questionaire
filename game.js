6cb23c1e789181c368cd387974bd4914cbcfffe3
Tirths101 soni <tirthsoni56@gmail.com>
2024-05-14 14:20:29 +0000

    updating changes

--- game.js
+++ game.js
@@ -76,10 +76,8 @@ choices.forEach((choice) => {
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
 
-        const classToApply = 'incorrect';
-          if (selectedAnswer == currentQuestion.answer) {
-            classToApply = 'correct'
-          }
+        const classToApply =
+          selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
         if (classToApply === "correct") {
             incrementScore(CORRECT_BONUS);
